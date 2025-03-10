9515bc977d2755e662926b9ad50e0129e5d84e96
Chairat008 <43720@prot.ac.th>
2025-03-10 11:40:43 +0700

    Add files via upload

---
+++ cell01/ex11/name.py
@@ -0,0 +1,9 @@
+# กำหนดตัวแปรชื่อและนามสกุล
+first_name = "Chairat"
+last_name = "Appakarat"
+
+# นำมาต่อกันแล้วเก็บในตัวแปร whole_name
+whole_name = last_name + " " + first_name
+
+# แสดงผล whole_name ตามด้วยขึ้นบรรทัดใหม่
+print(whole_name, "\n")
