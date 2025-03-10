107fa90b1a5707decf8cc95b0a2b243ec7850e44
Chairat008 <43720@prot.ac.th>
2025-03-10 12:06:57 +0700

    Add files via upload

---
+++ cell02/ex20/iszero.py
@@ -0,0 +1,8 @@
+# รับค่าตัวเลขจากผู้ใช้
+number = float(input("Enter a number: "))
+
+# ตรวจสอบว่าเลขนั้นเป็นศูนย์หรือไม่
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
