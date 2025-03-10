67dbf3789773eeabb2624a7d223be2b8145b4b9d
Chairat008 <43720@prot.ac.th>
2025-03-10 11:51:05 +0700

    Add files via upload

---
+++ cell01/ex13/whatsyourname.py
@@ -0,0 +1,6 @@
+# รับค่าชื่อจากผู้ใช้
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# แสดงชื่อและนามสกุล
+print("Your name is:", first_name, last_name, "\n")
